f6b77772c0473a4241b9d30dd6b1b2aa1616c6da
samfancy <samfancy2001@hotmail.com>
2024-09-15 20:41:42 +0100

    initial

---
+++ WebApp.UnitTest/UnitTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApp.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UnitTestShouldAlwaysWorkTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void OnePlusTwoEqualThreeTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void OnePlusTwoEqualFourTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
